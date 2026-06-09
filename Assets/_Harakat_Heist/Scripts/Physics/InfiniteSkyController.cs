f3db328b044e335199747d7c89dcacff8c148857
AmtunNoor <prideofmariam+github@gmail.com>
2026-06-09 17:00:19 +0300

    Create InfiniteSkyController.cs

---
+++ Assets/_Harakat_Heist/Scripts/Physics/InfiniteSkyController.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class InfiniteSkyController : MonoBehaviour
+{
+    // This script will handle our 360-degree looping backdrop
+    void Start()
+    {
+        Debug.Log("Harakat Heist: Infinite Sky system initialized.");
+    }
+}
